5213518511a2f8dfb68963f90ff773eea7da5889
kivio.wanderley <wkivio@gmail.com>
2018-03-04 14:52:06 -0300

    Adicionando Activity principal

--- app/src/main/java/com/example/wkivi/unipeimc/MainActivity.java
+++ app/src/main/java/com/example/wkivi/unipeimc/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.wkivi.unipeimc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextPeso;
+    private EditText editTextAltura;
+    private Button buttonCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        editTextPeso = findViewById(R.id.editTextPeso);
+        editTextAltura = findViewById(R.id.editTextAltura);
+        buttonCalcular = findViewById(R.id.buttonCalcular);
+
+        buttonCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String pesoString = String.valueOf(editTextPeso.getText());
+                String alturaString = String.valueOf(editTextAltura.getText());
+
+                if ( (null != pesoString && pesoString.length() > 0) && (null != alturaString && alturaString.length() > 0) )
+                {
+                    double peso = Double.parseDouble(pesoString);
+                    double altura = Double.parseDouble(alturaString);
+
+                    if (peso > 0 && altura > 0) {
+                        double resultado = peso / (Math.pow(altura, 2));
+
+                        Toast toast = Toast.makeText(getApplicationContext(), String.valueOf(resultado), Toast.LENGTH_LONG);
+                        toast.show();
+                    } else {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Valor Invalido", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+                else
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Erro de Entrada", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
     }
 }
