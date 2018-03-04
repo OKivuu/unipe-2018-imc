c938238ca33368454acb355348c493fd81c6434f
kivio.wanderley <wkivio@gmail.com>
2018-03-04 15:57:20 -0300

    Pedir pelo nome ao clicar em calcular

--- app/src/main/java/com/example/wkivi/unipeimc/MainActivity.java
+++ app/src/main/java/com/example/wkivi/unipeimc/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.wkivi.unipeimc;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +15,40 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextPeso;
     private EditText editTextAltura;
     private Button buttonCalcular;
+
+    private String m_Text = "";
+
+    private void inputDialog(String resultado)
+    {
+        final String imc = resultado;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Insira o seu nome:");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+
+                Toast toast = Toast.makeText(getApplicationContext(), m_Text+"\r\nIMC: "+imc, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +74,8 @@ public class MainActivity extends AppCompatActivity {
                     if (peso > 0 && altura > 0) {
                         double resultado = peso / (Math.pow(altura, 2));
 
-                        Toast toast = Toast.makeText(getApplicationContext(), String.valueOf(resultado), Toast.LENGTH_LONG);
-                        toast.show();
+                        inputDialog(String.valueOf(resultado));
+
                     } else {
                         Toast toast = Toast.makeText(getApplicationContext(), "Valor Invalido", Toast.LENGTH_LONG);
                         toast.show();
